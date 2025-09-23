c712c95a794e91a1505f0e9367e09b216bfe7b2f
sunwook <sw98@hwajintni.co.kr>
2025-09-23 12:51:12 +0900

    feat(bithumb_iota_krw_bot): 봇 예시 코드 추가
    - `IOTA/KRW`쌍 매매봇

---
+++ trading-bots/bithumb_iota_krw_bot.py
@@ -0,0 +1,24 @@
+﻿import ccxt
+import time
+
+# https://www.mycompiler.io/view/JZ8KnKHgyKZ 업로드 예정
+info = [
+    {"apiKey": "", "secret": ""},
+    {"apiKey": "", "secret": ""},
+    {"apiKey": "", "secret": ""},
+    {"apiKey": "", "secret": ""},
+    {"apiKey": "", "secret": ""},
+    {"apiKey": "", "secret": ""},
+]
+
+ticker = "IOTA/KRW"
+
+for i, account in enumerate(info):
+    exchange = ccxt.bithumb(account)
+
+    exchange.load_markets()
+
+    current_price = exchange.fetch_order_book(ticker, limit=1)["asks"][0][0]
+    amount = 5000  # current_price + 1
+
+    exchange.create_order()
