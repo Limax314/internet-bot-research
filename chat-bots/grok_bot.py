1d538d686cec2729e84ebd0910b195b8814d9c3f
Limax314 <thong31415@gmail.com>
2025-11-02 11:29:41 +0900

    feat(grok_bot.py): try 구문 추가 - 기능 구현 예정

--- chat-bots/grok_bot.py
+++ chat-bots/grok_bot.py
@@ -1,6 +1,12 @@
 def get_religion_score(user_id):
-    """_summary_
+    """@remarks
+    DocString 작성 예정
 
     Args:
         user_id (_type_): _description_
     """
+    try:
+        tweets=[]
+        next_token = None
+    # @remarks 여기서부터 작성 예정
+    except:
