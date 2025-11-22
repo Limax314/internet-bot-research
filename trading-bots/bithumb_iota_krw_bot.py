78d3b1336c17592e04f5f13642ccc15176781240
Limax314 <thong31415@gmail.com>
2025-11-22 19:46:51 +0900

    refactor(bithumb_iota_krw_bot.py): 파일 전체 Docstring 추가 - 각 코드별 주석도 추가 예정 - Python 문법도 정리 예정

--- trading-bots/bithumb_iota_krw_bot.py
+++ trading-bots/bithumb_iota_krw_bot.py
@@ -1,18 +1,28 @@
 ﻿import ccxt  # 암호화폐 거래소 트레이딩 라이브러리
 import time  # 시간 표준 라이브러리
 
-"""
-@remarks
-여기서부터 주석 추가 예정
-"""
-"""
-.. module:: bithumb_iota_krw_bot.py
-    :synopsis: Bithumb IOTA/KRW trading bot
+"""Bithumb IOTA/KRW 자동 거래 봇.
 
-Bithumb 거래소에서 여러 계정을 사용하여 IOTA/KRW 페어에 
-대해 지정된 수량만큼 시장가 매수 후 즉시 시장가 매도하는 
-스크립트입니다.
+@description
+- 이 스크립트는 Bithumb 거래소에서 IOTA/KRW 거래 쌍에 대한 자동 거래를 
+수행합니다.
+- 미리 설정된 여러 개의 계정 정보를 사용하여 각 계정에 대해 지정된 
+수량만큼 시장가 매수 주문을 실행한 후 즉시 동일한 수량으로 시장가 
+매도 주문을 실행합니다.
+- [주요 기능]:
+    - 다중 계정 지원(`info` 리스트에 여러 계쩡의 API 키와 시크릿 설정 
+    가능).
+    - 시장가 주문(`ccxt` 라이브러리를 사용하여 시장가 매수 및 매도 
+    주문 생성).
+    - 순차적 거래(설정된 계정 순서에 따라 순차적으로 매매 실행).
+- [주의사항]:
+    - `info` 리스트에 유효한 Bithumb API 키와 시크릿을 입력해야 합니다.
+    - `ticker` 변수에 거래를 원하는 거래 쌍을 설정합니다.
+    - `amount` 변수에 거래할 수량을 지정합니다.
 """
+"""@remarks
+여기서부터 주석 추가 예정
+    """
 info = [
     {"apiKey": "", "secret": ""},
     {"apiKey": "", "secret": ""},
@@ -24,14 +34,13 @@ info = [
 
 ticker = "IOTA/KRW"  # 거래할 페어
 
-# 정의된 모든 계정에 대해 거래를 실행합니다.
-for i, account in enumerate(info):
+for i, account in enumerate(info):  # 정의된 모든 계정에 대해 거래를 실행합니다.
     exchange = ccxt.bithumb(account)
 
     exchange.load_markets()
 
     current_price = exchange.fetch_order_book(ticker, limit=1)["asks"][0][0]
-    amount = 5000  # current_price + 1
+    amount = 5000
 
     exchange.create_order(ticker, type="market", side="buy", amount=amount)
 
